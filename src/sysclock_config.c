22aadaa6d33addd2a2f444bd0048609e3699a769
Siyuan Xu <siyuan.xu@outlook.com>
2023-02-24 15:27:17 +0200

    initial commit

---
+++ src/sysclock_config.c
@@ -0,0 +1,8 @@
+/*
+ * sysclock_config.c
+ *
+ *  Created on: Feb 24, 2023
+ *      Author: siyxu
+ */
+
+
---
+++ src/sysclock_config.h
@@ -0,0 +1,13 @@
+/*
+ * sysclock_config.h
+ *
+ *  Created on: Feb 24, 2023
+ *      Author: siyxu
+ */
+
+#ifndef SYSCLOCK_CONFIG_H_
+#define SYSCLOCK_CONFIG_H_
+
+
+
+#endif /* SYSCLOCK_CONFIG_H_ */
