8d451aca4325b5cc6dccf4841f7b31a5d0c31c4d
Siyuan Xu <siyuan.xu@outlook.com>
2023-03-01 10:41:16 +0200

    SGP30 ready

---
+++ src/SGP30.c
@@ -0,0 +1,298 @@
+#include "SGP30.h"
+#include "CRC.h"
+#include "I2C.h"
+#include "utils.h"
+
+/* SGP30 constants */
+const uint8_t Init_air_quality[2] = {0x20, 0x03};
+const uint8_t Measure_air_quality[2] = {0x20, 0x08};
+const uint8_t Get_baseline[2] = {0x20, 0x15};
+const uint8_t Set_baseline[2] = {0x20, 0x1e};
+const uint8_t Set_humidity[2] = {0x20, 0x61};
+const uint8_t Measure_test[2] = {0x20, 0x32};
+const uint8_t Get_feature_set_version[2] = {0x20, 0x2f};
+const uint8_t Measure_raw_signals[2] = {0x20, 0x50};
+const uint8_t Get_seiral_id[2] = {0x36, 0x82};
+
+/* Private function delcaration */
+static inline void s_SetCo2(sgp30_t *const sgp_data, const uint8_t *const sgp_binary_data) {
+    sgp_data->CO2 = ((uint16_t)sgp_binary_data[0] << 8) + (uint16_t)sgp_binary_data[1];
+    sgp_data->TVOC = ((uint16_t)sgp_binary_data[3] << 8) + (uint16_t)sgp_binary_data[4];
+}
+
+static inline void s_SetBaseline(sgp30_t *const sgp_data, const uint8_t *const sgp_binary_data) {
+    sgp_data->baselineCO2 = ((uint16_t)sgp_binary_data[0] << 8) + (uint16_t)sgp_binary_data[1];
+    sgp_data->baselineTVOC = ((uint16_t)sgp_binary_data[3] << 8) + (uint16_t)sgp_binary_data[4];
+}
+
+static inline void s_SetFeatureSet(sgp30_t *const sgp_data, const uint8_t *const sgp_binary_data) {
+    sgp_data->featureSetVersion = ((uint16_t)sgp_binary_data[0] << 8) + (uint16_t)sgp_binary_data[1];
+}
+
+static inline void s_SetRawData(sgp30_t *const sgp_data, const uint8_t *const sgp_binary_data) {
+    sgp_data->H2 = ((uint16_t)sgp_binary_data[0] << 8) + (uint16_t)sgp_binary_data[1];
+    sgp_data->ethanol = ((uint16_t)sgp_binary_data[3] << 8) + (uint16_t)sgp_binary_data[4];
+}
+
+static inline void s_SetSerialId(sgp30_t *const sgp_data, const uint8_t *const sgp_binary_data) {
+    sgp_data->serialID = ((uint64_t)sgp_binary_data[0] << 40) + ((uint64_t)sgp_binary_data[1] << 32) + ((uint64_t)sgp_binary_data[3] << 24) + ((uint64_t)sgp_binary_data[4] << 16) + ((uint64_t)sgp_binary_data[6] << 8) + ((uint64_t)sgp_binary_data[7]);
+}
+
+/* Public functions declaration */
+/**
+ * \brief Create a sgp30 object and initialise it
+ * \return Returns a sgp30 object with all fields initialised to 0
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ */
+sgp30_t sgp30_create() {
+    sgp30_t sgp30 = {0, 0, 0, 0, 0, 0, 0, 0};
+    return sgp30;
+}
+
+/**
+ * \brief Start air quality measurement. Initialization period takes about 15s.
+ * \return SGP30_SUCCESS
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details After InitAirQuality, MeasureAirQuality should be called in regular intervals of 1s. During initialization phase, returns fixed values of 400 ppm CO2eq and 0ppb TVOC.
+ */
+SGP30ERR sgp30_InitAirQuality() {
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Init_air_quality);
+    delay_ms(10);
+    I2C_Stop();
+    return SGP30_SUCCESS;
+}
+
+/**
+ * \brief Measure and calculate CO2eq and total VOC(TVOC)
+ * \param[out] sgp_data - The memory address where the date would be stored, 6 bytes.
+ * \return SGP30_SUCCESS, SGP30_ERR_BAD_CRC
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details After InitAirQuality, MeasureAirQuality should be called in regular intervals of 1s. During initialization phase, returns fixed values of 400 ppm CO2eq and 0ppb TVOC.
+ * For better accuracy, should SetBaseline according to GetBaseline.
+ */
+SGP30ERR sgp30_MeasureAirQuality(sgp30_t *const sgp_data) {
+    uint8_t crc_co2 = 0, crc_tvoc = 0;
+    uint8_t binary_data[6];
+
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Measure_air_quality);
+    delay_ms(12);
+    I2C_Read(SGP30_ADDR, 6, binary_data);
+
+    // CRC check
+    crc_co2 = CRC8(binary_data, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc_co2 != binary_data[2]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    crc_tvoc = CRC8(&binary_data[3], 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc_tvoc != binary_data[5]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    s_SetCo2(sgp_data, binary_data);
+
+    return SGP30_SUCCESS;
+}
+
+/**
+ * \brief Measure and calculate CO2eq and total VOC(TVOC) baseline.
+ * \param[out] sgp_data - The memory address where the date would be stored, 6 bytes.
+ * \return SGP30_SUCCESS, SGP30_ERR_BAD_CRC
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details For better accuracy, returned data from GetBaseline should be stored and SetBaseline every hour.
+ */
+SGP30ERR spg30_GetBaseLine(sgp30_t *const sgp_data) {
+    uint8_t crc_co2 = 0, crc_tvoc = 0;
+    uint8_t binary_data[6];
+
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Get_baseline);
+    delay_ms(10);
+    I2C_Read(SGP30_ADDR, 6, binary_data);
+
+    // CRC check
+    crc_co2 = CRC8(binary_data, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc_co2 != binary_data[2]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    crc_tvoc = CRC8(&binary_data[3], 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc_tvoc != binary_data[5]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    s_SetBaseline(sgp_data, binary_data);
+
+    return SGP30_SUCCESS;
+}
+
+/**
+ * \brief Set CO2eq and total VOC(TVOC) baseline for compensation algorithm.
+ * \param[in] baseline_eco2 - The baseline value of CO2eq, ppm.
+ * \param[in] baseline_tvoc - The baseline value of TVOC, ppb.
+ * \return none
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details For better accuracy, returned data from GetBaseline should be stored and SetBaseline every hour.
+ */
+void sgp30_SetBaseline(const uint16_t baseline_eco2, const uint16_t baseline_tvoc) {
+    uint8_t binary_data[6];
+    binary_data[0] = baseline_eco2 >> 8;
+    binary_data[1] = baseline_eco2 & 0x0f;
+    binary_data[2] = CRC8(binary_data, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    binary_data[3] = baseline_tvoc >> 8;
+    binary_data[4] = baseline_tvoc & 0x0f;
+    binary_data[5] = CRC8(binary_data + 3, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Set_baseline);
+    I2C_WriteData(6, binary_data);
+    delay_ms(10);
+    I2C_Stop();
+}
+
+/**
+ * \brief Set humidity compensation for the air quality signals (CO2eq and TVOC) and sensor raw signals (H2-signal and Ethanol_signal).
+ * \param[in] humidity - The absolute humidity of the environment.
+ * \return none
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details The 2 data bytes represent humidity values as a fixed-point 8.8bit number with a minimum value of 0x0001 (=1/256 g/m3) and a maximum value of 0xFFFF (255 g/m3 + 255/256 g/m3).
+ * For instance, sending a value of 0x0F80 corresponds to a humidity value of 16.50 g/m3(16 g/m3 + 128/256 g/m3).
+ * After setting a new humidity value, this value will be used by the on-chip humidity compensation algorithm until a new humidity value is set using the â€œSet_humidityâ€� command. Restarting
+ * the sensor (power-on or soft reset) or sending a value of 0x0000 (= 0 g/m3) sets the humidity value used for compensation to its default value (0x0B92 = 11.57 g/m3) until a new humidity
+ * value is sent. Sending a humidity value of 0x0000 can therefore be used to turn off the humidity compensation.
+ */
+void sgp30_SetHumidity(const uint16_t humidity) {
+    uint8_t binary_data[3];
+    binary_data[0] = humidity >> 8;
+    binary_data[1] = humidity & 0x0f;
+    binary_data[2] = CRC8(binary_data, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Set_baseline);
+    I2C_WriteData(3, binary_data);
+    delay_ms(10);
+    I2C_Stop();
+}
+
+/**
+ * \brief The command â€œMeasure_testâ€� which is included for integration and production line testing runs an on-chip self-test.
+ * \return SGP30_SUCCESS, SGP30_ERR_BAD_CRC
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details In case of a successful self-test the sensor returns the fixed binary_data pattern 0xD400 (with correct CRC).
+ */
+SGP30ERR sgp30_MeasureTest() {
+    uint8_t crc = 0;
+    uint8_t binary_data[3];
+
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Measure_test);
+    delay_ms(220);
+    I2C_Read(SGP30_ADDR, 3, binary_data);
+
+    // CRC check
+    crc = CRC8(binary_data, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc != binary_data[2]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    return SGP30_SUCCESS;
+}
+
+/**
+ * \brief The SGP30 features a versioning system for the available set of measurement commands and on-chip algorithms.
+ * \param[out] sgp_data - The memory address where the date would be stored, 3 bytes.
+ * \return SGP30_SUCCESS, SGP30_ERR_BAD_CRC
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details The sensor responds with 2 data bytes (MSB first) and 1 CRC byte.
+ */
+SGP30ERR sgp30_GetFeatureSetVersion(sgp30_t *const sgp_data) {
+    uint8_t crc = 0;
+    uint8_t binary_data[3];
+
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Get_feature_set_version);
+    delay_ms(2);
+    I2C_Read(SGP30_ADDR, 3, binary_data);
+
+    // CRC check
+    crc = CRC8(binary_data, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc != binary_data[2]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    s_SetFeatureSet(sgp_data, binary_data);
+
+    return SGP30_SUCCESS;
+}
+
+/**
+ * \brief Returns the sensor raw signals which are used as inputs for the on-chip calibration and baseline compensation algorithms.
+ * \param[out] sgp_data - The memory address where the date would be stored, 6 bytes.
+ * \return SGP30_SUCCESS, SGP30_ERR_BAD_CRC
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details The measurement to which the sensor responds with 2 data bytes (MSB first) and 1 CRC byte. for 2 sensor raw signals in the order H2_signal (sout_H2) and Ethanol_signal (sout_EthOH).
+ */
+SGP30ERR sgp30_MeasureRawSignals(sgp30_t *const sgp_data) {
+    uint8_t crc_h2 = 0, crc_ethanol = 0;
+    uint8_t binary_data[6];
+
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Measure_raw_signals);
+    delay_ms(25);
+    I2C_Read(SGP30_ADDR, 6, binary_data);
+
+    // CRC check
+    crc_h2 = CRC8(binary_data, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc_h2 != binary_data[2]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    crc_ethanol = CRC8(&binary_data[3], 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc_ethanol != binary_data[5]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    s_SetRawData(sgp_data, binary_data);
+
+    return SGP30_SUCCESS;
+}
+
+/**
+ * \brief The readout of the serial ID register can be used to identify the chip and verify the presence of the sensor.
+ * \param[out] sgp_data - The memory address where the date would be stored, 9 bytes.
+ * \return SGP30_SUCCESS, SGP30_ERR_BAD_CRC
+ * \author siyuan xu, e2101066@edu.vamk.fi, Jan.2023
+ * \details The get serial ID command returns 3 words, and every word is followed by an 8-bit CRC checksum. Together the 3 words constitute a unique serial ID with a length of 48 bits.
+ * The ID returned with this command are represented in the big endian (or MSB first) format.
+ */
+SGP30ERR sgp30_GetSerialId(sgp30_t *const sgp_data) {
+    uint8_t crc = 0;
+    uint8_t binary_data[9];
+
+    I2C_StartTransmission(SGP30_ADDR);
+    I2C_WriteCommand(2, Get_seiral_id);
+    delay_ms(5);
+    I2C_Read(SGP30_ADDR, 9, binary_data);
+
+    // CRC check
+    crc = CRC8(binary_data, 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc != binary_data[2]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    crc = CRC8(&binary_data[3], 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc != binary_data[5]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    crc = CRC8(&binary_data[6], 2, SGP30_CRC8_POLY, SGP30_CRC8_INIT, SGP30_CRC8_XOR);
+    if (crc != binary_data[8]) {
+        return SGP30_ERR_BAD_CRC;
+    }
+
+    s_SetSerialId(sgp_data, binary_data);
+
+    return SGP30_SUCCESS;
+}
---
+++ src/SGP30.h
@@ -0,0 +1,53 @@
+#ifndef SGP30_H
+#define SGP30_H
+
+/* SGP30 I2C addresses */
+#define SGP30_ADDR (uint8_t)0x58
+
+/* SGP30 command addresses */
+#define I2C_RESET_COMMAND (uint8_t)0x06
+
+/* SGP30 default values*/
+#define MEASURE_TEST_OK (uint16_t)0xd400
+
+/* CRC-8 in SGP30 */
+#define SGP30_CRC8_POLY (uint8_t)0x31  // x^8 + x^5 + x^4 + 1
+#define SGP30_CRC8_INIT (uint8_t)0xff
+#define SGP30_CRC8_XOR (uint8_t)0x00
+
+/* SGP30 data structure */
+#define SGP30_MSB 0
+#define SGP30_LSB 1
+
+typedef enum {
+    SGP30_SUCCESS = 0,
+    SGP30_ERR_BAD_CRC,
+    SGP30_SELF_TEST_FAIL
+} SGP30ERR;
+
+typedef struct sgp30_type sgp30_t;
+
+struct sgp30_type {
+    uint16_t CO2;
+    uint16_t TVOC;
+    uint16_t baselineCO2;
+    uint16_t baselineTVOC;
+    uint16_t featureSetVersion;
+    uint16_t H2;
+    uint16_t ethanol;
+    uint64_t serialID;
+};
+
+/* SGP30 function prototypes */
+sgp30_t sgp30_create(void);
+SGP30ERR sgp30_InitAirQuality(void);
+SGP30ERR sgp30_MeasureAirQuality(sgp30_t *const sgp_data);
+SGP30ERR spg30_GetBaseLine(sgp30_t *const sgp_data);
+void sgp30_SetBaseline(const uint16_t baseline_eco2, const uint16_t baseline_tvoc);
+void spg30_SetHumidity(const uint16_t humidity);
+SGP30ERR sgp30_MeasureTest(void);
+SGP30ERR sgp30_GetFeatureSetVersion(sgp30_t *const sgp_data);
+SGP30ERR sgp30_MeasureRawSignals(sgp30_t *const sgp_data);
+SGP30ERR sgp30_GetSerialId(sgp30_t *const sgp_data);
+
+#endif
