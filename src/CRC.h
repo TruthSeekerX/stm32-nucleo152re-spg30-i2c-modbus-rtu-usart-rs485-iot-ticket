abc6abb7f5388b4e0424ae02353279654bf47f3f
Siyuan Xu <siyuan.xu@outlook.com>
2023-03-01 10:29:55 +0200

    CRC-8 and CRC-modus added

---
+++ src/CRC.h
@@ -0,0 +1,9 @@
+#ifndef CRC_H
+#define CRC_H
+#include <stddef.h>
+#include <stdint.h>
+
+uint8_t CRC8(const uint8_t *data, const size_t length, const uint8_t polynomial, const uint8_t crc_init, const uint8_t final_xor);
+uint16_t CRC16(const char *nData, const uint16_t wLength);
+
+#endif
