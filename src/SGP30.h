ff5a41940d7de3f12a6d47489bcdde152b8be2f0
Siyuan Xu <siyuan.xu@outlook.com>
2023-03-08 10:54:56 +0200

    SGP30 driver update

--- src/SGP30.h
+++ src/SGP30.h
@@ -19,7 +19,13 @@
 #define SGP30_MSB 0
 #define SGP30_LSB 1
 
-typedef enum { SGP30_SUCCESS = 0, SGP30_ERR_BAD_CRC, SGP30_SELF_TEST_FAIL } SGP30ERR;
+typedef enum {
+    SGP30_SUCCESS = 0,
+    SGP30_ERR_BAD_CRC,
+    SGP30_SELF_TEST_FAIL,
+    SGP30_BAD_HUMIDITY,
+    SGP30_BAD_BASELINE
+} SGP30ERR;
 
 typedef struct sgp30_type sgp30_t;
 
@@ -39,8 +45,8 @@ sgp30_t  sgp30_create(void);
 SGP30ERR sgp30_InitAirQuality(void);
 SGP30ERR sgp30_MeasureAirQuality(sgp30_t *const sgp_data);
 SGP30ERR spg30_GetBaseLine(sgp30_t *const sgp_data);
-void     sgp30_SetBaseline(const uint16_t baseline_eco2, const uint16_t baseline_tvoc);
-void     spg30_SetHumidity(const uint16_t humidity);
+SGP30ERR sgp30_SetBaseline(const uint16_t baseline_eco2, const uint16_t baseline_tvoc);
+SGP30ERR spg30_SetAbsoluteHumidity(const double humidity);
 SGP30ERR sgp30_MeasureTest(void);
 SGP30ERR sgp30_GetFeatureSetVersion(sgp30_t *const sgp_data);
 SGP30ERR sgp30_MeasureRawSignals(sgp30_t *const sgp_data);
