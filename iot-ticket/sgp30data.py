8b371d789483a1d8640619b4168a6f901c39f7ec
Siyuan Xu <siyuan.xu@outlook.com>
2023-03-23 13:42:44 +0200

    added baseline to debug console

--- iot-ticket/sgp30data.py
+++ iot-ticket/sgp30data.py
@@ -4,7 +4,6 @@ import sys
 import time
 import random
 import serial
-import time
 from iotticket.models import device
 from iotticket.models import criteria
 from iotticket.models import deviceattribute
@@ -25,23 +24,23 @@ c = Client(baseurl, username, password)
 def send_data_to_iot_ticket(v1,v2):
     print("WRITE DEVICE DATANODES FUNTION.")
     listofvalues=[]
-    nv = datanodesvalue()
-    nv.set_name("CO2eq") #needed for writing datanode
-    nv.set_path("co2eq")
-    nv.set_dataType("long")
-    nv.set_unit("ppm")
-    nv.set_value(v1) #needed for writing datanode
-    nv.set_timestamp(int(round(time.time() * 1000)))
-
     nv1 = datanodesvalue()
-    nv1.set_name("TVOC") #needed for writing datanode
-    nv1.set_path("tvoc")
+    nv1.set_name("CO2eq") #needed for writing datanode
+    nv1.set_path("co2eq")
     nv1.set_dataType("long")
-    nv1.set_unit("ppb")
-    nv1.set_value(v2) #needed for writing datanode
-    nv1.set_timestamp(int(round(time.time() * 1000)))
-    listofvalues.append(nv)
+    nv1.set_unit("ppm")
+    nv1.set_value(v1) #needed for writing datanode
+    nv1.set_timestamp(int(round(time.time() * 1000)))    # Have to explicitly set timestamp
+
+    nv2 = datanodesvalue()
+    nv2.set_name("TVOC") #needed for writing datanode
+    nv2.set_path("tvoc")
+    nv2.set_dataType("long")
+    nv2.set_unit("ppb")
+    nv2.set_value(v2) #needed for writing datanode
+    nv2.set_timestamp(int(round(time.time() * 1000)))
     listofvalues.append(nv1)
+    listofvalues.append(nv2)
     print(c.writedata(deviceId, *listofvalues)) # another way to make this would be c.writedata(deviceId, nv, nv1)
     print("END WRITE DEVICE DATANODES FUNCTION")
     print("-------------------------------------------------------\n")
@@ -59,14 +58,25 @@ while True:
     r = ser_usart1.read(10)
     err += r[1]
     co2eq = (r[3] << 8) + r[4]
-    print("co2eq:", co2eq)
+    print("CO2eq:", co2eq)
     ser_usart1.write(commands["Tvoc"])
     r = ser_usart1.read(10)
     err += r[1]
     tvoc = (r[3] << 8) + r[4]
-    print("tvoc:", tvoc)
+    print("tVOC:", tvoc)
     
-    if err == 8:    #some error checking here. Because if error reply function code is not 4 but 4 + 0x80 = 132
+    ser_usart1.write(commands["BaseCo2eq"])
+    r = ser_usart1.read(10)
+    err += r[1]
+    baseCo2eq = (r[3] << 8) + r[4]
+    print("Base CO2eq:", baseCo2eq)
+    ser_usart1.write(commands["BaseTvoc"])
+    r = ser_usart1.read(10)
+    err += r[1]
+    baseTvoc = (r[3] << 8) + r[4]
+    print("Base tTVOC:", baseTvoc)
+    
+    if err == (4*4):    #some error checking here. Because if error reply function code is not 4 but 4 + 0x80 = 132
         send_data_to_iot_ticket(co2eq,tvoc)
     
     ser_usart1.close()
